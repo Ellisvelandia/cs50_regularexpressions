86b1f1d5ba462eba10ef7dedfeb59e70ee73a9e8
Ellisvelandia <eyis619l@gmail.com>
2023-08-07 22:00:51 -0500

    re.search

--- twitter.py
+++ twitter.py
@@ -1,9 +1,13 @@
-url = input("URL: ").strip()
-print(url)
+import re
 
+url = input("URL: ").strip()
+# print(url)
 # username = url.replace("https://twitter.com", "")
-username = re.sub("^(https?://)?(www\.)?twitter\.com/", "", url)
-print(f"Username: {username}")
+# username = re.sub("^(https?://)?(www\.)?twitter\.com/", "", url)
+# print(f"Username: {username}")
 
 #re.sub
 
+if matches := re.search(r"^htpps?://(?:www\.)?twitter\.com/(.+)$", url , re.IGNORECASE):
+# if matches:
+  print(f"Username:", matches.group(1))
